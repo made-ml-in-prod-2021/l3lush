340d85e7b168832d6627d344e1b5205f500c5cf8
l3lush <ananasnya@gmail.com>
2021-06-09 23:56:38 +0300

    working script

--- airflow_ml_dags/dags/02_train_validate_model.py
+++ airflow_ml_dags/dags/02_train_validate_model.py
@@ -13,23 +13,19 @@ TARGET_PATH = "/opt/airflow/data/raw/{{ ds }}/target.csv"
 
 with DAG(dag_id='train_validation_model',
          start_date=days_ago(3), schedule_interval="@weekly") as dag:
-    features_sensor = FileSensor(task_id='feature sensor', poke_interval=5, retries=3, filepath=FEATURES_PATH)
-    target_sensor = FileSensor(task_id='target sensor', poke_interval=5, retries=3, filepath=TARGET_PATH)
+    features_sensor = FileSensor(task_id='feature_sensor', poke_interval=5, retries=3, filepath=FEATURES_PATH)
+    target_sensor = FileSensor(task_id='target_sensor', poke_interval=5, retries=3, filepath=TARGET_PATH)
 
-    preprocessing = DockerOperator(image='airflow-preprocess', task_id='feature preprocessing', do_xcom_push=False,
-                                   command='/data/raw/{{ ds }} /data/processed/{{ ds }}',
+    preprocessing = DockerOperator(image='airflow-preprocess', task_id='feature_preprocessing', do_xcom_push=False,
                                    volumes=['C:/Users/anana/Desktop/MADE_2_sem/prod_ml/l3lush/airflow_ml_dags/data:/data'])
-    split = DockerOperator(image='airflow-split-data', task_id='splitting date', do_xcom_push=False,
-                           command='/data/processed/{{ ds }}',
+    split = DockerOperator(image='airflow-split-data', task_id='splitting_date', do_xcom_push=False,
                            volumes=['C:/Users/anana/Desktop/MADE_2_sem/prod_ml/l3lush/airflow_ml_dags/data:/data'])
-    train = DockerOperator(image='airflow-train', task_id='train model', do_xcom_push=False,
-                           command='/data/processed/{{ ds }} /data/models/{{ ds }}',
+    train = DockerOperator(image='airflow-train', task_id='train_model', do_xcom_push=False,
                            volumes=['C:/Users/anana/Desktop/MADE_2_sem/prod_ml/l3lush/airflow_ml_dags/data:/data'])
 
-    validate = DockerOperator(image='airflow-validate', task_id='model validation', do_xcom_push=False,
-                              command='/data/processed/{{ ds }} /data/models/{{ ds }}',
+    validate = DockerOperator(image='airflow-validate', task_id='model_validation', do_xcom_push=False,
                               volumes=['C:/Users/anana/Desktop/MADE_2_sem/prod_ml/l3lush/airflow_ml_dags/data:/data'])
 
-    log = BashOperator(task_id='report about ending', bash_command='echo "trained and validated"')
+    log = BashOperator(task_id='report_about_ending', bash_command='echo "trained and validated"')
 
     [features_sensor, target_sensor] >> preprocessing >> split >> train >> validate >> log
--- airflow_ml_dags/dags/03_make_prediction.py
+++ airflow_ml_dags/dags/03_make_prediction.py
@@ -10,12 +10,11 @@ DEFAULT_SCALER_PATH = '/opt/airflow/data/processed/{{ ds }}/scaler.csv'
 
 
 with DAG(dag_id='prediction', start_date=days_ago(3), schedule_interval="@daily") as dag:
-    data_sensor = FileSensor(task_id='data sensor', poke_interval=3, retries=3, filepath=DEFAULT_DATA_PATH)
-    model_sensor = FileSensor(task_id='model sensor', poke_interval=3, retries=3, filepath=DEFAULT_MODEL_PATH)
-    scaler_sensor = FileSensor(task_id='scaler sensor', poke_interval=3, retries=3, filepath=DEFAULT_SCALER_PATH)
+    data_sensor = FileSensor(task_id='datasensor', poke_interval=3, retries=3, filepath=DEFAULT_DATA_PATH)
+    model_sensor = FileSensor(task_id='modelsensor', poke_interval=3, retries=3, filepath=DEFAULT_MODEL_PATH)
+    scaler_sensor = FileSensor(task_id='scalersensor', poke_interval=3, retries=3, filepath=DEFAULT_SCALER_PATH)
 
     prediction = DockerOperator(image='airflow-predict', task_id='prediction', do_xcom_push=False,
-                                command='/data/processed/{{ ds }} /data/models/{{ ds }}',
                                 volumes=['C:/Users/anana/Desktop/MADE_2_sem/prod_ml/l3lush/airflow_ml_dags/data:/data'])
 
     [data_sensor, model_sensor, scaler_sensor] >> prediction
